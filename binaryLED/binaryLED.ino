b2ea3e5ffed99128f3f5a41f90f56c92937e8c8f
Stephen Pope <stevempope@gmail.com>
2019-06-09 16:22:23 +0100

    Updated Pinmode intialisation

--- binaryLED/binaryLED.ino
+++ binaryLED/binaryLED.ino
@@ -15,32 +15,18 @@
  */
 
 //8 bit number length for v0.1
- #define PIN0 0
- #define PIN1 1
- #define PIN2 2
- #define PIN3 3
- #define PIN4 4
- #define PIN5 5
- #define PIN6 6
- #define PIN7 7
+#define maxPins 8
 
 /*
  * The setup function runs just the once. This should be used to set the intial values and intialise
  * any digital/analog pins. For more information, see: https://www.arduino.cc/reference/en/
  */
 void setup() {
-  pinMode(PIN0, OUTPUT);
-  pinMode(PIN1, OUTPUT);
-  pinMode(PIN2, OUTPUT);
-  pinMode(PIN3, OUTPUT);
-  pinMode(PIN4, OUTPUT);
-  pinMode(PIN5, OUTPUT);
-  pinMode(PIN6, OUTPUT);
-  pinMode(PIN7, OUTPUT);
-  //There is a more elegant way : https://forum.arduino.cc/index.php?topic=49548.0
-}
+  Serial.begin(9600);
+  for(int i = 0; i < maxPins; i++){
+    pinMode(i, OUTPUT);
+    }
+  }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
