9f51e82ef1a3def0488814ac44ea38095f9eb823
Stephen Pope <stevempope@gmail.com>
2019-06-09 17:10:52 +0100

    Added bitReading and parsing
    parseInt captures positive ints only and bitRead can turn the pins on and off, looping works correctly

--- binaryLED/binaryLED.ino
+++ binaryLED/binaryLED.ino
@@ -14,19 +14,41 @@
  * #Define is used to give the pins a more memorable value than simply their number
  */
 
-//8 bit number length for v0.1
+//32 bit is the maximum length for a long int
+//8 bit positive int only for v0.1
 #define maxPins 8
 
 /*
  * The setup function runs just the once. This should be used to set the intial values and intialise
  * any digital/analog pins. For more information, see: https://www.arduino.cc/reference/en/
+ * 
+ * Serial baudrate is set to 9600 for compatibility. The rate bits need to be accepted at is low for
+ * this application, so this does not require changing.
  */
+ 
+long recieved;
+
 void setup() {
   Serial.begin(9600);
+  Serial.setTimeout(100000); //This prevents the status of any LED being overwritten too quickly. 100,000 ms must elapse before the LED's will turn off.
   for(int i = 0; i < maxPins; i++){
     pinMode(i, OUTPUT);
-    }
   }
+}
 
 void loop() {
+  if (Serial.available() > 0){
+    recieved = Serial.parseInt();
+    for(int i = 0; i < maxPins; i++){
+      if(bitRead(recieved, i) == 0){
+        Serial.print(bitRead(recieved, i));
+        digitalWrite(i, LOW);
+      }
+      else{
+        Serial.print(bitRead(recieved, i));
+        digitalWrite(i, HIGH);
+      }
+    }
+    Serial.println();
+    }
 }
